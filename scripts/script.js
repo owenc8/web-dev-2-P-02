2c7007df165fdb3c163b68a1f073411037df4e9c
Owen Chan <git@owenc.ca>
2025-12-05 10:52:26 -0800

    starter javascript for rotating images

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// const img = document.querySelector('img'); // Select your image
+// let rotation = 0;
+
+// setInterval(() => {
+//   rotation += 90;
+//   img.style.transform = `rotate(${rotation}deg)`;
+// }, 1000);
+
+//put in css
+// img {
+//   transition: transform 0.5s ease;
+// }
